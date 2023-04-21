b5965b3020ea765c464b3a12810405089e9919b4
baku67 <basile08@hotmail.fr>
2023-04-21 15:54:09 +0200

    WIP Pokémons types (+affichage)

--- pokeApi.js
+++ pokeApi.js
@@ -16,24 +16,231 @@ window.onload = function() {
     function getInfosAPI(nbrPoke) {
         const contentDiv = document.querySelector("#content");
 
-        fetch("https://pokeapi.co/api/v2/pokemon?limit=" + nbrPoke + "&offset=0")
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
 
-            contentDiv.innerHTML = "";
+        contentDiv.innerHTML = "";
+        for (let i = 1; i < nbrPoke; i++) {
+            
+            fetch("https://pokeapi.co/api/v2/pokemon/"+ i)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
 
-            data.results.forEach(element => {
+                let capitalizedName = capitalize(data.forms[0].name);
+
+                // Gestion du/des types du Pokémon:
+
+                var color = "";
+                var color1 = "";
+                var color2 = "";
+                var bgColor = "";
+
+                if (data.types.length == 1) {
+                    // var typeClass = data.types[0].type.name;
+                    // var typeClass2 = "";
+
+                    switch (data.types[0].type.name) {
+
+                        case "fire":
+                            color = "#ff7600";
+                            break;
+                        case "water":
+                            color = "#0072ff";
+                            break;
+                        case "poison":
+                            color = "#224e06";
+                            break;
+                        case "grass":
+                            color = "#00ff66";
+                            break;
+                        case "normal":
+                            color = "#808080";
+                            break;
+                        case "electric":
+                            color = "#EABB00";
+                            break;
+                        case "ice":
+                            color = "#BDFFF8";
+                            break;
+                        case "fighting":
+                            color = "#FF8C2E";
+                            break;
+                        case "ground":
+                            color = "#863D18";
+                            break;
+                        case "flying":
+                            color = "#56CFED";
+                            break;
+                        case "psychic":
+                            color = "#AA2DDC";
+                            break;
+                        case "bug":
+                            color = "#9FE320";
+                            break;
+                        case "rock":
+                            color = "#6F6057";
+                            break;
+                        case "ghost":
+                            color = "#E7DBED";
+                            break;
+                        case "dragon":
+                            color = "darkBlue";
+                            break;
+                        case "steel":
+                            color = "gray";
+                            break;
+                        case "dark":
+                            color = "black";
+                            break;
+                        case "fairy":
+                            color = "pink";
+                            break;
+                    }
+                    bgColor =  "background-color:"+ color;
+
+
+                }
+                else if (data.types.length == 2) {
+                    // var typeClass = "";
+                    // var typeClass2 = data.types[0].type.name + " " + data.types[1].type.name;
+                    
+                    switch (data.types[0].type.name) {
+
+                        case "fire":
+                            color1 = "#ff7600";
+                            break;
+                        case "water":
+                            color1 = "#0072ff";
+                            break;
+                        case "poison":
+                            color1 = "#224e06";
+                            break;
+                        case "grass":
+                            color1 = "#00ff66";
+                            break;
+                        case "normal":
+                            color1 = "#808080";
+                            break;
+                        case "electric":
+                            color1 = "#EABB00";
+                            break;
+                        case "ice":
+                            color1 = "#BDFFF8";
+                            break;
+                        case "fighting":
+                            color1 = "#FF8C2E";
+                            break;
+                        case "ground":
+                            color1 = "#863D18";
+                            break;
+                        case "flying":
+                            color1 = "#56CFED";
+                            break;
+                        case "psychic":
+                            color1 = "#AA2DDC";
+                            break;
+                        case "bug":
+                            color1 = "#9FE320";
+                            break;
+                        case "rock":
+                            color1 = "#6F6057";
+                            break;
+                        case "ghost":
+                            color1 = "#E7DBED";
+                            break;
+                        case "dragon":
+                            color1 = "darkBlue";
+                            break;
+                        case "steel":
+                            color1 = "gray";
+                            break;
+                        case "dark":
+                            color1 = "black";
+                            break;
+                        case "fairy":
+                            color1 = "pink";
+                            break;
+                    }
+
+                    switch (data.types[1].type.name) {
+
+                        case "fire":
+                            color2 = "#ff7600";
+                            break;
+                        case "water":
+                            color2 = "#0072ff";
+                            break;
+                        case "poison":
+                            color2 = "#224e06";
+                            break;
+                        case "grass":
+                            color2 = "#00ff66";
+                            break;
+                        case "normal":
+                            color2 = "#808080";
+                            break;
+                        case "electric":
+                            color2 = "#EABB00";
+                            break;
+                        case "ice":
+                            color2 = "#BDFFF8";
+                            break;
+                        case "fighting":
+                            color2 = "#FF8C2E";
+                            break;
+                        case "ground":
+                            color2 = "#863D18";
+                            break;
+                        case "flying":
+                            color2 = "#56CFED";
+                            break;
+                        case "psychic":
+                            color2 = "#AA2DDC";
+                            break;
+                        case "bug":
+                            color2 = "#9FE320";
+                            break;
+                        case "rock":
+                            color2 = "#6F6057";
+                            break;
+                        case "ghost":
+                            color2 = "#E7DBED";
+                            break;
+                        case "dragon":
+                            color2 = "darkBlue";
+                            break;
+                        case "steel":
+                            color2 = "gray";
+                            break;
+                        case "dark":
+                            color2 = "black";
+                            break;
+                        case "fairy":
+                            color2 = "pink";
+                            break;
+                    }
+                    bgColor =  "background: linear-gradient(132deg, "+ color1 +" 0%, "+ color2 +" 100%);" ;
+                }
+
+                // console.log(typeClass);
+
+
+
                 contentDiv.innerHTML += 
-                "<div>" +
-                    "<span>" + element.name + "</span>" +
-                    // "<img src='" + element.url + "'>" +
-                    "<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + 1 + ".png' >" + 
+                "<div style='"+bgColor+"' class='pokeCard'>" +
+                    "<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + data.id + ".png' >" + 
+                    "<span>" + capitalizedName + "</span>" +
                 "</div>"
-            });
 
-        })
-        .catch((error) => contentDiv.innerHTML = error)
+            })
+            .catch((error) => contentDiv.innerHTML = error)
+            
+        }
+
+    }
+
+
+    function capitalize(word) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
     }
 
 
