cdc7cbe84300eac080a363a22067ed884d32e3ec
baku67 <basile08@hotmail.fr>
2023-04-21 09:36:34 +0200

    Ex1 API (géoGouv)

---
+++ script.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+
+    const form = document.querySelector("form")
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const inputCodePostal = document.querySelector(".codePostal")
+        getInfosAPI(inputCodePostal);
+    })
+
+    function getInfosAPI(inputCodePostal) {
+        const result = document.querySelector(".result")
+        fetch("https://geo.api.gouv.fr/communes?codePostal="+inputCodePostal.value+"&fields=nom,code,codesPostaux,population")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            result.innerHTML = "Code INSEE : " + data[0].code + "<br>"
+            result.innerHTML += "Nom : " + data[0].nom + "<br>"
+            result.innerHTML += "La liste des codes postaux est : <ul>"
+            data[0].codesPostaux.forEach((codePostal) => {
+                result.innerHTML += "<li style='list-style-type:none'>" + codePostal + "</li>"
+            });
+            result.innerHTML += "</ul><br>"
+            result.innerHTML += "Population : " + data[0].population + " habitants<br>"
+        })
+        .catch((error) => result.innerHTML = error)
+    }
+}
+
